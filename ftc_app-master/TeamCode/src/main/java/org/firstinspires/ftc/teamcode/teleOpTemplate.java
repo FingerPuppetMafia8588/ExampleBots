a0dca9bbab4b27a310b467e0a86e8b2812211728
Isaac Blandin Mac <myung0966@gmail.com>
2018-03-09 14:59:12 -0600

    added a template for coding teleop

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpTemplate.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by isaac.blandin on 3/9/18.
+ */
+
+@TeleOp(name = "name here")
+@Disabled
+public class teleOpTemplate extends LinearOpMode {
+
+    //declare hardware here
+    DcMotor motorName;
+    Servo servoName;
+
+    @Override
+    public void runOpMode(){
+        //all code for during initialization here
+
+        motorName = hardwareMap.dcMotor.get("motor name");
+        servoName = hardwareMap.servo.get("servo name");
+
+        motorName.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorName.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //all code after the start is pressed after this term
+        waitForStart();
+
+        while (opModeIsActive()){
+            //all code for during main teleop loop here
+
+            motorName.setPower(gamepad1.left_stick_y);
+
+            if (gamepad1.a) {
+                servoName.setPosition(1);
+            } else if (gamepad1.b){
+                servoName.setPosition(0);
+            }
+
+        }
+        stop();
+    }
+}
